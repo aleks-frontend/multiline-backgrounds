6b87add15c5cf15e6c7b0d37c570852bb2c6c08c
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2019-07-26 14:17:42 +0200

    chore: added functionality to add strong text

--- multilineBackgrounds.js
+++ multilineBackgrounds.js
@@ -1,38 +1,43 @@
 // Adding background for the multiline text blocks
 function multilineBackgrounds() {
-  const multilineBlocks = document.querySelectorAll('.js-padded-multiline-src');
-  multilineBlocks.forEach(multilineBlock => {
-    const originalContent = multilineBlock.innerHTML;
-    const hideUrlStart = ( multilineBlock.dataset.hideWww == 'hide' );
-    let formated = originalContent.replace(/<br>/gi, '[linebreak]');
-    let tempDiv = document.createElement('div');
-    tempDiv.innerHTML = formated.trim();
-    const split = tempDiv.innerText.trim().split("[linebreak]");
-    let target;          
-    let output = "";
-    let zIndex = split.length;
-    
-    
-    split.forEach((line, i) => {
-      // Checking if we need to hide the url beginning
-      if ( i == 0 && hideUrlStart ) line = line.split('www.')[1];              
-      
-      (line == '') ? output += `<span class="emptyLine"></span>` : output += `<span>${line}</span>`;
-      if (i < split.length - 1) output += "<br>";
-      
-      zIndex--;
+    const multilineBlocks = document.querySelectorAll('.js-padded-multiline-src');
+    multilineBlocks.forEach(multilineBlock => {
+        const originalContent = multilineBlock.innerHTML;
+        let formated = originalContent
+            .replace(/<br>/gi, '[linebreak]')
+            .replace(/<strong>/gi, '[strong-open]')
+            .replace(/<\/strong>/gi, '[strong-close]');
+        let tempDiv = document.createElement('div');
+        tempDiv.innerHTML = formated.trim();
+        const split = tempDiv.innerText.trim().split("[linebreak]");
+        let target;          
+        let output = "";
+        let zIndex = split.length;
+        split.forEach((line, i) => {
+            const finalLine = line
+                .replace(/\[strong-open\]/gi, '<strong>')
+                .replace(/\[strong-close\]/gi, '</strong>');
+            (finalLine == '') ? output += `<span class="emptyLine"></span>` : output += `<span>${finalLine}</span>`;
+          if (i < split.length - 1) {
+            output += "<br>";
+          }
+          zIndex--;
+        });
+
+        // Since we can't add unique IDs in collection items, we create specific case
+        // 'data-target-id' is removed from the 'js-padded-multiline-src' element and 
+        // 'js-padded-multiline-target' is added to the target element instead of an ID
+        // it is very important in this case that both src and target elements are on the same level
+        if ( document.querySelector(`#${multilineBlock.dataset.targetId}`) == undefined ) {
+          target = multilineBlock.parentNode.querySelector('.js-padded-multiline-target');
+        } else {
+          target = document.querySelector(`#${multilineBlock.dataset.targetId}`);
+        }          
+
+        target.innerHTML = `
+        <div class="padded-multiline-bg">${output}</div>
+        <div class="padded-multiline-fg">${output}<div>`;
     });
-
-    // Since we can't add unique IDs in collection items, we create specific case
-    // 'data-target-id' is removed from the 'js-padded-multiline-src' element and 
-    // 'js-padded-multiline-target' is added to the target element instead of an ID
-    // it is very important in this case that both src and target elements are on the same level
-    if ( document.querySelector(`#${multilineBlock.dataset.targetId}`) == undefined ) {
-      target = multilineBlock.parentNode.querySelector('.js-padded-multiline-target');
-    } else {
-      target = document.querySelector(`#${multilineBlock.dataset.targetId}`);
-    }          
-
-    target.innerHTML = `<div class="padded-multiline-bg">${output}</div><div class="padded-multiline-fg">${output}<div>`;
-  });
 }
+
+multilineBackgrounds();
