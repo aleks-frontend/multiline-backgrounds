744b23e439d9c4847fb59160b31bde99706f1e2a
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2019-07-31 11:29:48 +0200

    fix: added a fix for the scenario when strong tag is opened in one line and cloded in another

--- multilineBackgrounds.js
+++ multilineBackgrounds.js
@@ -11,21 +11,34 @@ function multilineBackgrounds() {
             .replace(/<\/span>/gi, '[underline-close]');
         let tempDiv = document.createElement('div');
         tempDiv.innerHTML = formated.trim();
-        const split = tempDiv.innerText.trim().split("[linebreak]");
+        const split = tempDiv.innerText.trim().split('[linebreak]');
         let target;
-        let output = "";
-        let zIndex = split.length;
+        let output = '';
         split.forEach((line, i) => {
             const finalLine = line
                 .replace(/\[strong-open\]/gi, '<strong>')
                 .replace(/\[strong-close\]/gi, '</strong>')
                 .replace(/\[underline-open\]/gi, '<span style="text-decoration: underline;">')
                 .replace(/\[underline-close\]/gi, '</span>');
-            (finalLine == '') ? output += `<span class="emptyLine"></span>` : output += `<span>${finalLine}</span>`;
-          if (i < split.length - 1) {
-            output += "<br>";
-          }
-          zIndex--;
+
+            const strongOpenCount = (finalLine.match(/<strong>/g) || []).length;
+            const strongCloseCount = (finalLine.match(/<\/strong>/g) || []).length;
+            let strongOpenTag = '';
+            let strongCloseTag = '';
+
+            // Checking if a <strong> tag is opened in one line and closed in a different one
+            if ( strongOpenCount < strongCloseCount ) strongOpenTag = '<strong>';
+            if ( strongOpenCount > strongCloseCount ) strongCloseTag = '</strong>';
+
+            if (finalLine == '') {
+                output += '<span class="emptyLine"></span>';
+            } else {
+                output += `<span>${strongOpenTag + finalLine + strongCloseTag}</span>`;
+            }
+
+            if (i < split.length - 1) {
+                output += '<br>';
+            }
         });
 
         // Since we can't add unique IDs in collection items, we create specific case
@@ -33,14 +46,14 @@ function multilineBackgrounds() {
         // 'js-padded-multiline-target' is added to the target element instead of an ID
         // it is very important in this case that both src and target elements are on the same level
         if ( document.querySelector(`#${multilineBlock.dataset.targetId}`) == undefined ) {
-          target = multilineBlock.parentNode.querySelector('.js-padded-multiline-target');
+            target = multilineBlock.parentNode.querySelector('.js-padded-multiline-target');
         } else {
-          target = document.querySelector(`#${multilineBlock.dataset.targetId}`);
-        }          
+            target = document.querySelector(`#${multilineBlock.dataset.targetId}`);
+        }
 
         target.innerHTML = `
-        <div class="padded-multiline-bg">${output}</div>
-        <div class="padded-multiline-fg">${output}<div>`;
+            <div class="padded-multiline-bg">${output}</div>
+            <div class="padded-multiline-fg">${output}<div>`;
     });
 }
 
